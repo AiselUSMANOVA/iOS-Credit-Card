43e26599c56a5eef96ed777b996e62976b997908
AiselUSMANOVA <160107054@stu.sdu.edu.kz>
2021-01-23 22:52:55 +0600

    first commit

---
+++ Credit Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Credit Card
+//
+//  Created by Aisel Usmanova on 1/23/21.
+//  Copyright © 2021 Aisel Usmanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
